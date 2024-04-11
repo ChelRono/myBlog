dcf4a72d09dbd636c02fdcebde2ba48bd71035f6
Valarie <valarierono97@gmail.com>
2024-04-11 15:39:49 +0300

    Makes migrations of the blog app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
+    category = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+     
